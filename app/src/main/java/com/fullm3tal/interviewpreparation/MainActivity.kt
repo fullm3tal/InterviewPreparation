fc6570736711ab5764dd6935a727e525531f63ec
Bheem Negi <bheem.negi3@gmail.com>
2023-09-20 10:32:54 +0530

    pending Intent

--- app/src/main/java/com/fullm3tal/interviewpreparation/MainActivity.kt
+++ app/src/main/java/com/fullm3tal/interviewpreparation/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.fullm3tal.interviewpreparation
 
-import androidx.appcompat.app.AppCompatActivity
+
+import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationChannel: NotificationChannel
+    lateinit var builder: Notification.Builder
+    private val channelId = "i.apps.notifications"
+    private val description = "Test notification"
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val tvHelloWorld = findViewById<TextView>(R.id.tvHelloWorld)
+        tvHelloWorld.setOnClickListener {
+            Log.v("Tag", "Tag")
+            pendingIntent()
+        }
+    }
+
+    private fun pendingIntent() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.journaldev.com/"))
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        // checking if android version is greater than oreo(API 26) or not
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel =
+                NotificationChannel(channelId, description, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val builder: NotificationCompat.Builder = NotificationCompat.Builder(this, "Hello")
+        builder.setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT) // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        notificationManager.notify(1234, builder.build())
     }
 }
